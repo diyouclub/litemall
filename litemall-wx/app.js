741b5f9f4e8918a10a1cd8a6d92b6e860d5f8529
张怡锋 <zzq674@icloud.com>
2019-01-18 09:43:37 +0800

    11

--- litemall-wx/app.js
+++ litemall-wx/app.js
@@ -11,7 +11,7 @@ App({
         content: '新版本已经准备好，是否重启应用？',
         success: function(res) {
           if (res.confirm) {
-            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            // 新的版本已经下1载好，调用 applyUpdate 应用新版本并重启
             updateManager.applyUpdate()
           }
         }
